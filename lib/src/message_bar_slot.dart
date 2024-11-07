875b9fb3aeff00456cf74c60692e10d7a0efa6e8
Olivier Huin <olih@users.noreply.github.com>
2024-11-07 21:02:36 +0000

    Slot title in uppercase

--- lib/src/message_bar_slot.dart
+++ lib/src/message_bar_slot.dart
@@ -20,9 +20,9 @@ class BubblegumMessageBarSlot extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(Icons.info, color: Colors.blue),
+        Icon(Icons.markunread_sharp, color: Colors.blue),
         const SizedBox(width: 8),
-        Text(slot.title, style: TextStyle(fontWeight: FontWeight.bold)),
+        Text(slot.title.toUpperCase(), style: TextStyle(fontWeight: FontWeight.bold)),
         const Spacer(),
         Tooltip(message: slot.description, child: const Icon(Icons.help)),
         const SizedBox(width: 8),
