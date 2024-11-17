b29004549c92c1ef4d4180d5a04f083d76bc2d53
Olivier Huin <olih@users.noreply.github.com>
2024-11-17 22:37:24 +0000

    Follows a11y guidelines

--- test/message_badge_widget_test.dart
+++ test/message_badge_widget_test.dart
@@ -125,5 +125,39 @@ void main() {
       expect(
           icon.semanticLabel, options.messageLabelForLevel(messages[0].level));
     });
+
+    testWidgets('Follows a11y guidelines', (tester) async {
+      final SemanticsHandle handle = tester.ensureSemantics();
+      final messages = [
+        createCopperframeMessage(CopperframeMessageLevel.warning),
+      ];
+      final options = BubblegumMessageSlotOptsBuilder().build();
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BubblegumLevelBadgeWidget(
+            messages: messages,
+            options: options,
+          ),
+        ),
+      );
+
+      // Checks that tappable nodes have a minimum size of 48 by 48 pixels
+      // for Android.
+      await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+
+      // Checks that tappable nodes have a minimum size of 44 by 44 pixels
+      // for iOS.
+      await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+
+      // Checks that touch targets with a tap or long press action are labeled.
+      await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+
+      // Checks whether semantic nodes meet the minimum text contrast levels.
+      // The recommended text contrast is 3:1 for larger text
+      // (18 point and above regular).
+      await expectLater(tester, meetsGuideline(textContrastGuideline));
+      handle.dispose();
+    });
   });
 }
