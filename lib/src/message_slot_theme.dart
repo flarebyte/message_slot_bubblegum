e2fa5bccf6c1795fe64f7106f7515a8c1dffa46d
Olivier Huin <olih@users.noreply.github.com>
2024-11-19 03:27:20 +0000

    Adds tests for slot theme

--- lib/src/message_slot_theme.dart
+++ lib/src/message_slot_theme.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
+/// Enum representing different T-shirt sizes.
+///
+/// This is used for defining different sizes for UI elements such as box decorations.
 enum TShirtSize { extraSmall, small, medium, large, extraLarge }
 
+/// A constant map that defines the width of slot box decorations based on the T-shirt size.
+///
+/// The sizes range from extraSmall to extraLarge, each associated with a specific width.
 const slotBoxDecorationWidth = {
   TShirtSize.extraSmall: .5,
   TShirtSize.small: 1.0,
@@ -10,7 +16,13 @@ const slotBoxDecorationWidth = {
   TShirtSize.extraLarge: 6.0,
 };
 
+/// A theme utility class for defining the appearance of the Bubblegum message slot.
+///
+/// This class provides methods to get colors and decorations for message slots based on the current theme.
 class BubblegumMessageSlotTheme {
+  /// Returns the color to be used for a slot based on the current [themeData].
+  ///
+  /// If the theme brightness is light, it returns a shade of grey. Otherwise, it returns a white shade.
   static Color colorOfSlot(ThemeData themeData) {
     if (themeData.colorScheme.brightness == Brightness.light) {
       return Colors.grey.shade500;
@@ -19,6 +31,9 @@ class BubblegumMessageSlotTheme {
     }
   }
 
+  /// Returns the color to be used for the header divider based on the current [themeData].
+  ///
+  /// If the theme brightness is light, it returns a shade of grey. Otherwise, it returns a white shade.
   static Color colorOfHeaderDivider(ThemeData themeData) {
     if (themeData.colorScheme.brightness == Brightness.light) {
       return Colors.grey.shade500;
@@ -27,6 +42,9 @@ class BubblegumMessageSlotTheme {
     }
   }
 
+  /// Returns a [BoxDecoration] for a message slot based on the given [themeData] and [shirtSize].
+  ///
+  /// The decoration includes a border whose width is defined by the [shirtSize] and a rounded corner.
   static BoxDecoration getSlotBoxDecoration(
       {required ThemeData themeData, required TShirtSize shirtSize}) {
     return BoxDecoration(
